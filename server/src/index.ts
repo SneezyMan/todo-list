00ffc406c87967de68d7edd80174ec22437883c5
Kohtaroh <kohtaroh881@gmail.com>
2025-05-08 20:29:14 +1000

    Removed unused middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,7 @@
 import express, { Request, Response } from 'express';
-import dotenv from 'dotenv';
 import { AuthRegister } from './auth';
-import cors from 'cors';
-
-dotenv.config();
-
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
